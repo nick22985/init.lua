4e9033d31dfa46ee6282d6eae5e34fb6be55eb79
nick22985 <nick22985@hotmail.com>
2023-04-21 00:19:55 +1000

    feat: stuff

--- lua/nick22985/packer.lua
+++ lua/nick22985/packer.lua
@@ -23,11 +23,22 @@ return require('packer').startup(function(use)
 use "olimorris/onedarkpro.nvim"
 
 
+use({
+    "folke/trouble.nvim",
+    config = function()
+        require("trouble").setup {
+            --icons = false,
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    end
+})
+
 use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 use('nvim-treesitter/nvim-treesitter-context')
 
 use('nvim-treesitter/playground')
-
 use('theprimeagen/harpoon')
 use('mbbill/undotree')
 use('tpope/vim-fugitive')
@@ -53,5 +64,9 @@ use {
 }
 
 use('wakatime/vim-wakatime')
+
+use("github/copilot.vim")
+
+use("laytan/cloak.nvim")
 
 end)
