13d64e47abf115ae34dabbf2ab4d2f8e4c75abb6
nick22985 <nick22985@hotmail.com>
2024-01-06 08:33:30 +1000

    feat: update fzf to auto cmake

--- lua/nick22985/lazy.lua
+++ lua/nick22985/lazy.lua
@@ -21,7 +21,8 @@ local plugins = {
 			'nvim-lua/plenary.nvim',
 			{
 				'nvim-telescope/telescope-fzf-native.nvim',
-				build = 'make',
+				build =
+				'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
 			}
 		}
 	},
