bc82b8eb37959adf6316db9518340f3e626229be
nick22985 <nick@nick22985.com>
2025-01-29 13:52:52 +1000

    feat: nvim package updates

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -1,5 +1,8 @@
 ;; extends 
 
+(("and" @keyword.function) (#set! conceal "&"))
+(("return" @keyword.function) (#set! conceal "R"))
+(("then" @conditional) (#set! conceal "t"))
 (("else" @conditional) (#set! conceal "e"))
 (("elseif" @conditional) (#set! conceal "e"))
 (("end" @keyword.function) (#set! conceal "E"))
@@ -8,7 +11,4 @@
 (("if" @conditional) (#set! conceal "?"))
 (("in" @keyword) (#set! conceal "i"))
 ((function_call name: (identifier) @function.builtin (#eq? @function.builtin "require")) (#set! conceal "r"))
-(("then" @conditional) (#set! conceal "t"))
-(("and" @keyword.function) (#set! conceal "&"))
-(("return" @keyword.function) (#set! conceal "R"))
 (("do" @repeat) (#set! conceal "d"))
