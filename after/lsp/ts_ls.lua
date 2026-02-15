8e9e122d7d63c245448c2d2217368e263feb8f9f
nick22985 <nick@nick22985.com>
2026-02-15 12:05:41 +1000

    update ts_ls

--- after/lsp/ts_ls.lua
+++ after/lsp/ts_ls.lua
@@ -1,7 +1,17 @@
 return {
 
 	root_dir = function(bufnr, on_dir)
-		return nil
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		local lsp_utils = require("nick22985.utils.lsp-utils")
+		local util = require("lspconfig.util")
+		local root_dir = util.root_pattern("package.json", "vue.config.js")(fname)
+		local isVue3 = root_dir and lsp_utils.is_vue3_project(root_dir)
+		local isVue2 = root_dir and lsp_utils.is_vue2_project(root_dir)
+		if isVue3 or isVue2 then
+			return on_dir(root_dir)
+		else
+			return
+		end
 	end,
 	settings = {
 		analysis = {
